d8b30618426d1506ab80b5e5cc5c6756c5eab94c
CBurg-College <cburgmicro@outlook.com>
2025-09-06 20:59:16 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -55,31 +55,34 @@ enum Color {
     Purple
 }
 
-function fromColor(color: Color): number {
-    let val = 0
-    switch (color) {
-        case Color.Red: val = 0xFF0000; break;
-        case Color.Green: val = 0x00FF00; break;
-        case Color.Blue: val = 0x0000FF; break;
-        case Color.Yellow: val = 0xFFFF00; break;
-        case Color.Cyan: val = 0x00FFFF; break;
-        case Color.Magenta: val = 0xFF00FF; break;
-        case Color.Black: val = 0x000000; break;
-        case Color.DarkGrey: val = 0xA9A9A9; break;
-        case Color.Grey: val = 0x808080; break;
-        case Color.LightGrey: val = 0xD3D3D3; break;
-        case Color.White: val = 0xFFFFFF; break;
-        case Color.Orange: val = 0xFFA500; break;
-        case Color.Brown: val = 0xA52A2A; break;
-        case Color.Pink: val = 0xFFC0CB; break;
-        case Color.Indigo: val = 0x4b0082; break;
-        case Color.Violet: val = 0x8a2be2; break;
-        case Color.Purple: val = 0x800080; break;
-    }
-    return val
-}
+namespace Color {
 
-function fromRgb(red: number, green: number, blue: number): number {
-    let rgb = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF)
-    return rgb;
+    function fromColor(color: Color): number {
+        let val = 0
+        switch (color) {
+            case Color.Red: val = 0xFF0000; break;
+            case Color.Green: val = 0x00FF00; break;
+            case Color.Blue: val = 0x0000FF; break;
+            case Color.Yellow: val = 0xFFFF00; break;
+            case Color.Cyan: val = 0x00FFFF; break;
+            case Color.Magenta: val = 0xFF00FF; break;
+            case Color.Black: val = 0x000000; break;
+            case Color.DarkGrey: val = 0xA9A9A9; break;
+            case Color.Grey: val = 0x808080; break;
+            case Color.LightGrey: val = 0xD3D3D3; break;
+            case Color.White: val = 0xFFFFFF; break;
+            case Color.Orange: val = 0xFFA500; break;
+            case Color.Brown: val = 0xA52A2A; break;
+            case Color.Pink: val = 0xFFC0CB; break;
+            case Color.Indigo: val = 0x4b0082; break;
+            case Color.Violet: val = 0x8a2be2; break;
+            case Color.Purple: val = 0x800080; break;
+        }
+        return val
+    }
+
+    function fromRgb(red: number, green: number, blue: number): number {
+        let rgb = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF)
+        return rgb;
+    }
 }
